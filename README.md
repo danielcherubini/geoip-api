ee2a46e93dd50739819dbf943bccd76f0df2b9b9
Daniel Cherubini <daniel@cherubini.casa>
2017-05-26 14:57:44 +0200

    added basic install script and updated readme

---
+++ README.md
@@ -0,0 +1,12 @@
+[![Build Status](https://travis-ci.org/danmademe/geoip-api.svg?branch=master)](https://travis-ci.org/danmademe/geoip-api) [![Coverage Status](https://coveralls.io/repos/github/danmademe/geoip-api/badge.svg?branch=master)](https://coveralls.io/github/danmademe/geoip-api?branch=master)
+# geoip-api
+I convert ip's into countries and stuff
+
+```
+./geoip-api --lang languages.json
+```
+
+# Install using
+```sh
+curl -Ls https://raw.githubusercontent.com/danmademe/geoip-api/master/install.sh | sudo -H sh
+```
---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+FILE="/lib/systemd/system/geoip-api.service"
+
+geoipFile="geoip-api-linux-amd64"
+curl -s https://api.github.com/repos/danmademe/geoip-api/releases | grep browser_download_url | grep ${geoipFile} | head -n 1 | cut -d '"' -f 4 | wget -i -
+
+
+chmod +x ${geoipFile}
+mv ${geoipFile} /usr/local/bin/geoip-api
+
+cat > $FILE <<- EOM
+[Unit]
+Description=GEO IP to Locale
+
+[Service]
+Type=simple
+PIDFile=/tmp/geoip-api.pid-4040
+Restart=always
+ExecStart=/usr/local/bin/geoip-api -lang=/etc/geoip-api/languages.json
+
+[Install]
+WantedBy=multiuser.target
+EOM
+
+
+file="/etc/geoip-api/geoip-api.yml"
+if [ -f "$file" ]
+then
+	echo "$file found, skipping."
+else
+	echo "$file not found, adding."
+    sudo mkdir /etc/geoip-api
+    sudo touch /etc/geoip-api/languages.json
+    sudo chmod 777 /etc/geoip-api/languages.json
+fi
+
+sudo systemctl daemon-reload
+sudo systemctl start geoip-api.service
