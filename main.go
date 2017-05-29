2647a1767a648fae7b6e83680424d64f0d1099da
Daniel Cherubini <daniel@cherubini.casa>
2017-05-29 16:07:06 +0200

    changed it to bad request

--- main.go
+++ main.go
@@ -31,6 +31,8 @@ func CheckIPRoute(w http.ResponseWriter, r *http.Request) {
 		//Setup struct
 		err, responseObject := utils.GetLocale(r)
 		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
 			fmt.Fprintf(w, "{\"error\": \"%s\"}", err.Error())
 		} else {
 
