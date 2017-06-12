56b7e89817faf536e45f395081ac1ca61690e9ac
Daniel Cherubini <daniel@cherubini.casa>
2017-06-12 13:37:17 +0200

    removed test for download s3

--- services/services_test.go
+++ services/services_test.go
@@ -3,21 +3,33 @@ package services
 import (
 	"fmt"
 	"testing"
-
-	"github.com/danmademe/geoip-api/models"
 )
 
-func TestDownloadS3Url(t *testing.T) {
+// func TestDownloadS3Url(t *testing.T) {
+// 	fmt.Println("Testing DownloadS3Url")
+//
+// 	s3Config := models.S3Config{}
+// 	s3Config.Bucket = "tidal-bi-emr"
+// 	s3Config.Key = "/libs/playbacklog/GeoIP2-City.mmdb"
+// 	s3Config.Region = "us-east-1"
+//
+// 	filename := "GeoIP2-City.mmdb"
+//
+// 	err, filePath := DownloadS3Url(s3Config, filename)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		t.Fail()
+// 	}
+//
+// 	fmt.Println("Test Pass: " + filePath)
+// }
+
+func TestDownloadUrl(t *testing.T) {
 	fmt.Println("Testing DownloadS3Url")
+	urlString := "http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz"
+	filename := "GeoLite2-City.tar.gz"
 
-	s3Config := models.S3Config{}
-	s3Config.Bucket = "tidal-bi-emr"
-	s3Config.Key = "/libs/playbacklog/GeoIP2-City.mmdb"
-	s3Config.Region = "us-east-1"
-
-	filename := "GeoIP2-City.mmdb"
-
-	err, filePath := DownloadS3Url(s3Config, filename)
+	err, filePath := DownloadUrl(urlString, filename)
 	if err != nil {
 		fmt.Println(err)
 		t.Fail()
@@ -25,18 +37,3 @@ func TestDownloadS3Url(t *testing.T) {
 
 	fmt.Println("Test Pass: " + filePath)
 }
-
-//
-// func TestDownloadUrl(t *testing.T) {
-// fmt.Println("Testing DownloadS3Url")
-// 	urlString := "http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz"
-// 	filename := "GeoLite2-City.tar.gz"
-//
-// 	err, filePath := DownloadUrl(urlString, filename)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 	}
-//
-// fmt.Println("Test Pass: " + filePath)
-// }
