daf8588016dc96c3ce9c09e2802d1a79ee016a29
Daniel Cherubini <daniel@cherubini.casa>
2017-09-08 10:13:48 +0200

    enabled output of lookup in logs

--- main.go
+++ main.go
@@ -48,6 +48,7 @@ func CheckIPRoute(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
 			fmt.Fprintf(w, "%s", jsonString)
+			fmt.Println(jsonString)
 		}
 	}
 }
